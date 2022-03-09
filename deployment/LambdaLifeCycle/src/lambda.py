8ba20e3f95e8118f33f75adefa6ad4d1896ef9b5
Daniel Blake <dnblak@amazon.com>
2022-03-09 09:35:31 -0500

    Updated to try and get SM studio to have a lice cycle config. No luck, no support for AWS Labs. Fail in the end.

--- deployment/LambdaLifeCycle/src/lambda.py
+++ deployment/LambdaLifeCycle/src/lambda.py
@@ -1,3 +1,10 @@
+######
+# 
+# I was working on this to get life cycle configs setup and applied. The way it was only let one config be attached to the domain, and did not attach it to the user profile, so nothing worked. 
+# I was adding the updateUserProfile function and changing the updateSageMakerDomainNew function to account for this
+# Last test the updateSageMakerDomain function worked, however it did not allow Studio to launch an app.
+# 
+######
 import json, boto3, base64, uuid, logging
 import cfnresponse
 
@@ -7,10 +14,15 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 responseData = {}
 
-def lambda_handler(event, context):
+def handler(event, context):
   logger.info(event)
-  
   if event['RequestType'] == 'Create':
+    LifeCycleConfigs = getLifeCycleConfigs(event, context, 'Lab')['StudioLifecycleConfigs']
+    # if there is an existing lifecycle config, then we should bail. Else, create them
+    if len(LifeCycleConfigs) > 0:
+      logger.error('Too many configs. Deleting the old.')
+      deleteConfig(event, context, 'Lab')
+
     logger.debug("Create")
     ConfigContentRawShutdown = """
                     # Installs SageMaker Studio's Auto Shutdown Idle Kernel Sessions extension
@@ -30,9 +42,7 @@ def lambda_handler(event, context):
                     # restarts jupter server
                     nohup supervisorctl -c /etc/supervisor/conf.d/supervisord.conf restart jupyterlabserver
                     """
-    createConfig(event, ConfigContentRawShutdown,'StudioAutoShutdown', 'JupyterServer')
-    updateSageMakerDomain(event, event['ResourceProperties'].get('DomainId'), 'StudioAutoShutdown', 'JupyterServerAppSettings')
-    
+
     bucket = 'dnblak-dev1'
     region = 'us-west-2'
     solutionName = 'sagemaker-battlesnake-ai/source'
@@ -59,7 +69,7 @@ def lambda_handler(event, context):
                     echo '}}' >> stack_outputs.json
                     '''.format(bucket=bucket,
                                 region=region,
-                                solutionName=solutionNam,
+                                solutionName=solutionName,
                                 AccountId=AccountId, 
                                 S3BucketName=S3BucketName, 
                                 SageMakerIamRoleArn=SageMakerIamRoleArn, 
@@ -68,13 +78,18 @@ def lambda_handler(event, context):
                                 SagemakerTrainingInstanceType=SagemakerTrainingInstanceType, 
                                 SagemakerInferenceInstanceType=SagemakerInferenceInstanceType)
 
-    createConfig(event, ConfigContentRawCopySetup,'RawCopySetup')
-    updateSageMakerDomain(event, event['ResourceProperties'].get('DomainId'), 'RawCopySetup')
+    createConfig(event, context, ConfigContentRawShutdown,'LabStudioAutoShutdown', 'JupyterServer')
+    createConfig(event, context, ConfigContentRawCopySetup,'LabRawCopySetup')
+    updateSageMakerDomainNew(event, context, event['ResourceProperties'].get('DomainId'), 'Lab')
+    
+    # updateSageMakerDomain(event, context, event['ResourceProperties'].get('DomainId'), 'LabStudioAutoShutdown', 'JupyterServerAppSettings')
+    # # updateUserProfile(event, context, event['ResourceProperties'].get('DomainId'), 'LabStudioAutoShutdown')
+    # updateSageMakerDomain(event, context, event['ResourceProperties'].get('DomainId'), 'LabRawCopySetup')
 
   elif event['RequestType'] == 'Delete':
     logger.debug("Delete")
-    deleteConfig(event, 'StudioAutoShutdown')
-    deleteConfig(event, 'RawCopySetup')
+    deleteConfig(event, context, 'StudioAutoShutdown')
+    deleteConfig(event, context, 'RawCopySetup')
 
   responseData['Data'] = 'Success'
   cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData, event["LogicalResourceId"])
@@ -83,21 +98,22 @@ def lambda_handler(event, context):
     'body': json.dumps('LifeCycle configs taken care of')
   }
 
-def getLifeCycleConfigs(event, NameContains):
+def getLifeCycleConfigs(event, context, NameContains):
   try:
     listResponse = SageMaker.list_studio_lifecycle_configs(NameContains=NameContains)
+    logger.info(json.dumps(listResponse, default=str))
     return listResponse
   except Exception as e:
     logger.error(f'Unable to list Studio LifeCycle Configs: {e}')
     responseData['Data'] = f'LifeCycle Configs not found: {e}'
     cfnresponse.send(event, context, cfnresponse.FAILED, responseData, event["LogicalResourceId"]);
 
-def deleteConfig(event, NameContains):
-  listResponse = getLifeCycleConfigs(event, NameContains)
+def deleteConfig(event, context, NameContains):
+  listResponse = getLifeCycleConfigs(event, context, NameContains)
   logger.debug(listResponse)
   
   for config in listResponse['StudioLifecycleConfigs']:
-    print(config['StudioLifecycleConfigName'])
+    logger.debug(config['StudioLifecycleConfigName'])
     try: 
       deleteResponse = SageMaker.delete_studio_lifecycle_config(StudioLifecycleConfigName=config['StudioLifecycleConfigName'])
       logger.debug(deleteResponse)
@@ -107,7 +123,7 @@ def deleteConfig(event, NameContains):
       cfnresponse.send(event, context, cfnresponse.FAILED, responseData, event["LogicalResourceId"]);
 
 # StudioLifecycleConfigAppType: JupyterServer|KernelGateway
-def createConfig(event, configContentRaw,StudioLifecycleConfigName,StudioLifecycleConfigAppType='KernelGateway'):
+def createConfig(event, context, configContentRaw,StudioLifecycleConfigName,StudioLifecycleConfigAppType='KernelGateway'):
   # Base64 encode the script (required)
   configContent = B64Encode(configContentRaw)
   # Create a UUID to prevent name collisions
@@ -131,26 +147,70 @@ def B64Encode(configContentRaw):
   configContentB64 = base64.b64encode(configContentASCII)
   configContent = configContentB64.decode('ascii')
   return configContent
-  
+
 # AppType: JupyterServerAppSettings|KernelGatewayAppSettings
-def updateSageMakerDomain(event, DomainId, NameContains, AppType='KernelGatewayAppSettings'):
-  LifeCycleConfigs = getLifeCycleConfigs(event, NameContains)['StudioLifecycleConfigs']
-  if count(LifeCycleConfigs) > 1:
+def updateSageMakerDomainNew(event, context, DomainId, NameContains, AppType='KernelGatewayAppSettings'):
+  LifeCycleConfigs = getLifeCycleConfigs(event, context, NameContains)['StudioLifecycleConfigs']
+  logger.info('In the new domain updater')
+  logger.info(len(LifeCycleConfigs))
+  for config in LifeCycleConfigs:
+    logger.info(config)
+    # try:
+    #   updateResponse = SageMaker.update_domain(
+    #     DomainId=DomainId,
+    #     DefaultUserSettings={
+    #       AppType: {
+    #         'DefaultResourceSpec': {
+    #           'InstanceType': 'system',
+    #           'LifecycleConfigArn': LifeCycleConfigs[0]['StudioLifecycleConfigArn']
+    #         }
+    #       }
+    #     }
+    #   )
+    # except Exception as e:
+    #   logger.error(f'Adding lifecycle failed. {e}')
+    #   responseData['Data'] = 'The LifeCycle was not properly applied. did not update.'
+    #   cfnresponse.send(event, context, cfnresponse.FAILED, responseData, event["LogicalResourceId"]);
+
+# AppType: JupyterServerAppSettings|KernelGatewayAppSettings
+def updateSageMakerDomain(event, context, DomainId, NameContains, AppType='KernelGatewayAppSettings'):
+  LifeCycleConfigs = getLifeCycleConfigs(event, context, NameContains)['StudioLifecycleConfigs']
+  if len(LifeCycleConfigs) > 1:
     responseData['Data'] = f'Too many LifeCycle Configs found.'
     cfnresponse.send(event, context, cfnresponse.FAILED, responseData, event["LogicalResourceId"]);
   else:
     try:
-      updateResponse = sagemaker.update_domain(
+      updateResponse = SageMaker.update_domain(
         DomainId=DomainId,
         DefaultUserSettings={
           AppType: {
             'DefaultResourceSpec': {
-              'LifecycleConfigArn': LifecycleConfigArn
+              'InstanceType': 'system',
+              'LifecycleConfigArn': LifeCycleConfigs[0]['StudioLifecycleConfigArn']
             }
           }
         }
       )
     except Exception as e:
-      logger.error(f'Adding lifecycle failed.{e}')
+      logger.error(f'Adding lifecycle failed. {e}')
       responseData['Data'] = 'The LifeCycle was not properly applied. did not update.'
       cfnresponse.send(event, context, cfnresponse.FAILED, responseData, event["LogicalResourceId"]);
+
+
+def updateUserProfile(event, context, DomainId, NameContains):
+  LifeCycleConfigs = getLifeCycleConfigs(event, context, NameContains)['StudioLifecycleConfigs']
+
+  try: 
+    updateResponse = SageMaker.update_user_profile(
+      DomainId=DomainId,
+      UserProfileName='a',
+      UserSettings={
+        'JupyterServerAppSettings': {
+            'LifecycleConfigArns': [LifeCycleConfigs[0]['StudioLifecycleConfigArn']]
+        }
+      }
+    )
+  except Exception as e:
+    logger.error(f'Adding lifecycle to user-profile failed. {e}')
+    responseData['Data'] = 'The LifeCycle was not properly applied to the user profile. did not update.'
+    cfnresponse.send(event, context, cfnresponse.FAILED, responseData, event["LogicalResourceId"]);
